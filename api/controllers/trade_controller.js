68cb7b5f69eaa36a16d7d26b13261b614333c48e
Georgi Urumov <gurumov92@gmail.com>
2017-03-09 22:50:37 +0100

    fixed trade controller

--- api/controllers/trade_controller.js
+++ api/controllers/trade_controller.js
@@ -8,11 +8,11 @@ module.exports = {
 
 function submitSellRequest(req, res, next) {
     try {
-        var accountAddress = req.swagger.params.sellRequest.accountAddress.value;
-        var password =  req.swagger.params.sellRequest.password.value;
-        var volume = Number(req.swagger.params.sellRequest.volume.value);
-        if (typeof req.swagger.params.sellRequest.price.value !== "undefined" && req.swagger.params.sellRequest.price.value != null ) {
-            var price = Number(req.swagger.params.sellRequest.price.value);
+        var accountAddress = req.swagger.params.sellRequest.value.accountAddress;
+        var password =  req.swagger.params.sellRequest.value.password;
+        var volume = Number(req.swagger.params.sellRequest.value.volume);
+        if (typeof req.swagger.params.sellRequest.value.price !== "undefined" && req.swagger.params.sellRequest.value.price != null ) {
+            var price = Number(req.swagger.params.sellRequest.value.price);
         } else {
             res.statusCode = 500;
             res.end("A price is required to submit your selling order!")
@@ -29,11 +29,11 @@ function submitSellRequest(req, res, next) {
 
 function submitBuyRequest(req, res, next) {
      try {
-        var accountAddress = req.swagger.params.buyRequest.accountAddress.value;
-        var password =  req.swagger.params.buyRequest.password.value;
-        var volume = Number(req.swagger.params.buyRequest.volume.value);
-        if (typeof req.swagger.params.buyRequest.price.value !== "undefined" && req.swagger.params.buyRequest.price.value != 0) {
-            var price = Number(req.swagger.params.buyRequest.price.value);
+        var accountAddress = req.swagger.params.buyRequest.value.accountAddress;
+        var password =  req.swagger.params.buyRequest.value.password;
+        var volume = Number(req.swagger.params.buyRequest.value.volume);
+        if (typeof req.swagger.params.buyRequest.value.price !== "undefined" && req.swagger.params.buyRequest.value.price != 0) {
+            var price = Number(req.swagger.params.buyRequst.value.price);
         } else {
             var price = Number.MAX_VALUE;
         }
