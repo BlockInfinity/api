df44fd388a567ea0580c849cb54e212b309998f4
mgsgde <mgsgde@mgsgde@gmail.com>
2017-03-14 12:51:05 +0100

    mysql timeout error fixed

--- api/controllers/exchangeAPI/eventWatcher.js
+++ api/controllers/exchangeAPI/eventWatcher.js
@@ -6,12 +6,42 @@ const etherex = web3.exchangeContract;
 var currPeriod = etherex.getCurrPeriod().toNumber();
 var state = etherex.getCurrState().toNumber();
 
-var connection = mysql.createConnection({
+var db_config ={
     host: "localhost",
     user: "dex",
     password: "amalien",
     database: "apidb"
-});
+};
+
+
+// in order to avoid time out errors and establish a contionous connection
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+setInterval(function () {
+    connection.query('SELECT 1');
+}, 5000);
 
 // inserts the matching price into the database when the state changes to 1
 var StateChangeEvent = etherex.StateChangedEvent();
@@ -92,3 +122,6 @@ module.exports = {
     getPeriod: getPeriod,
     getState: getState
 }
+
+
+
