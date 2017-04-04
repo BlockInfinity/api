cd8e9f1f35c8549cc917aef099df10669d60ea59
mgsgde <mgsgde@mgsgde@gmail.com>
2017-04-04 18:53:55 +0200

    contract

--- api/controllers/exchangeAPI/chainConnector.js
+++ api/controllers/exchangeAPI/chainConnector.js
@@ -8,7 +8,7 @@ try {
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
     web3.eth.defaulAccount = web3.eth.accounts[0];
 
-    var contractAddress = '0xdeb5af9335f48b020b529207ddafe50b5bcc9eaa';
+    var contractAddress = '0x8916d85d377fd0484b57bfa1ee375900d7ca72f7';
     var compiled;
     var abi;
 
